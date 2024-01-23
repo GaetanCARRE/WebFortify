80bad23ff76b5029545cd6d1fc64f0ab907b8acd
LeZhuck <101577633+LeZhuck@users.noreply.github.com>
2024-01-23 12:46:08 +0100

    feat : find file in project automated. find vulnerable parameters + similarity with the url targeted

--- back-web-fortify/corrections/correction_xsstrike.py
+++ back-web-fortify/corrections/correction_xsstrike.py
@@ -1,5 +1,7 @@
 import os
 import re
+import json
+import difflib
 
 def find_xss_strike(path, form_parameters):
     xss_vuln = []
@@ -47,13 +49,52 @@ def add_file_content(file_path, line_number, line_content, param, xss_vuln):
     })
     return xss_vuln
 
-# Exemple d'utilisation
-path = r'C:\wamp64\www\WebFortify'  # Utilisez 'r' avant le chemin pour éviter les caractères d'échappement
-form_parameters = [{'param': 'name="title"'}, {'param': 'name="content"'}]
-result = find_xss_strike(path, form_parameters)
-print(f"Found {len(result)} files containing the specified form parameters:")
-for item in result:
-    print(f"File: {item['file_path']}")
-    for line_entry in item['lines']:
-        print(f"  Line: {line_entry['number']}, Param: {line_entry['param']}, Content: {line_entry['line_content']}")
+def condition_multiple_links(result, url) :
+    if(len(result) > 0) :
+        for item in result:
+            if(item['file_path'] == url) :
+                return True
+    
+def similarity_ratio(a, b):
+    return difflib.SequenceMatcher(None, a, b).ratio()
+
+def condition_multiple_links(result, url):
+    best_match = None
+    best_ratio = 0.0
+
+    for item in result:
+        file_path = item['file_path']
+        ratio = similarity_ratio(file_path, url)
+        
+        # Choisissez le meilleur match avec la plus grande similarité
+        if ratio > best_ratio:
+            best_ratio = ratio
+            best_match = item
+
+    if best_match:
+        return True, best_match
+    else:
+        return False, None
+
+def main_correction(project_path) : 
+    vulnerabilities = []
+    with open('./lib/XSStrike/result-XSS-Strike.json', 'r') as json_file:
+        vulnerabilities = json.load(json_file)
+    for vulnerability in vulnerabilities:
+        url = vulnerability['url']
+        list_parameters = []
+        for list_vulnerability in vulnerability['list_vulnerability']:
+            list_parameters.append({'param' : 'name="'+list_vulnerability['parameter']+ '"'})
+        result = find_xss_strike(project_path, list_parameters)
+        file_path = ""
+        if len(result) > 1:
+            is_match, file_path = condition_multiple_links(result, url)
+            if is_match:
+                file_path = file_path['file_path']
+            else:
+                print("No matching file found.")         
+        elif len(result) == 1:
+            file_path = result[0]['file_path']
+        print(result)
+     
     
