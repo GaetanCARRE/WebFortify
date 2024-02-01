defc0fbb410320e0d703481a7377eb9dc1186d93
LeZhuck <101577633+LeZhuck@users.noreply.github.com>
2024-02-01 11:42:37 +0100

    feat : add correction file for xss

---
+++ site-test/pages/correction_xss.php
@@ -0,0 +1,58 @@
+
+<?php
+header ("X-XSS-Protection: 0");
+
+$html = "";
+// Is there any input?*
+
+if( isset( $_GET[ 'title' ])  && isset( $_GET[ 'content' ] ) ) {
+    // Get input
+
+    $title = htmlspecialchars($_GET[ 'title' ], ENT_QUOTES, 'UTF-8');
+    $content = htmlspecialchars($_GET[ 'content' ], ENT_QUOTES, 'UTF-8');
+ 
+    // Feedback for end user
+    $html .= "<pre>Your comment has been posted!</pre>";
+    
+    // Display feedback
+    $html .= "<pre>Title: {$title}</pre>";
+    $html .= "<pre>Content: {$content}</pre>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>XSS Page </title>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<meta charset='UTF-8'>
+<meta http-equiv='Content-Security-Policy' content="default-src 'self'; script-src 'self';">
+<link rel="stylesheet" type="text/css" href="xss_correction.css">
+
+</head>
+<body>
+    <div class="main-div">
+    <img src="../img/logo.png" alt="Test Image" height="200" width="300" >
+    <header>
+                <button onclick="window.location.href = 'home.php';">Home</button>
+                <button onclick="window.location.href = 'brute-force.php';">Brute Force</button>
+                <button onclick="window.location.href = 'sql-injection.php';">SQL Injection</button>
+                <button onclick="window.location.href = 'file_upload.php';">File Upload</button>
+            </header>
+    <h1>XSS Page</h1> 
+    <form method="get" action="correction_xss.php">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required><br><br>
+        
+        <label for="content">Content:</label><br>
+        <textarea id="content" name="content" rows="5" cols="40" required></textarea><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+    <?php echo $html; ?>  <!-- Affiche le contenu de la variable $html -->
+    </div>
+</body>
+</html>
+
